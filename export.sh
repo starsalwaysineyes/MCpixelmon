5458f86856a95f9705cc52e5e50cfe6367e4063b
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2024-01-17 19:05:44 +0000

    update

---
+++ export.sh
@@ -0,0 +1,11 @@
+./node_modules/.bin/ngrok config add-authtoken 2b4pxijYq22mOZT3wQexCKuPX4j_2L7aCWNfKXCoaaoVkhA9p
+# ./node_modules/.bin/ngrok config add-authtoken 2b5CjFWtALNkAei5junEwN0qLqg_79sJeACX1cp856FYWLN4n
+# ./node_modules/.bin/ngrok config add-authtoken 2b5D8zQ3IAsvLtKuI7Xc3z5qm63_4Z2hXXtFsrEwoaMsdMLHy
+# ./node_modules/.bin/ngrok config add-authtoken 2b5DMKAE61y6uSAzndr8XqaJGXA_2Nf5zQXC1bihyEqeq4MNS
+# ./node_modules/.bin/ngrok config add-authtoken 2b5DSLZOp7xgPSS4ibX63bc97vm_3f7j6ogVAbBVudbGCkwj8
+# ./node_modules/.bin/ngrok config add-authtoken 2b5Dh5KY0GbVBJrndAsPdfHzzpT_4bejUkuvQN1QuqhsMcR8v
+# ./node_modules/.bin/ngrok config add-authtoken 2b5DywFLrEPpCJsHfWtBAjVnjJP_bu7y8LTbu5aF2NXNvu2U
+# ./node_modules/.bin/ngrok config add-authtoken 2b5ECEgiIHOOkwyTG3EVtInP2YF_2Yv6HH1JyBgE9euE5D2rF
+# ./node_modules/.bin/ngrok config add-authtoken 2b5EQ4AYVHbX9PQgN9yT2EqZEVa_4rjynh4jzaB1NZ3sYs2Qk
+# ./node_modules/.bin/ngrok config add-authtoken 2b5EvykR8U85hVM3lofFWOdgUCh_4nwWcceeSz8CqKzDp2Nxx
+./node_modules/.bin/ngrok tcp 25565
