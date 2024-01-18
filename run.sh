0c3f32937578d4b4179d9582a2c2af6ff8eb2146
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2024-01-18 10:26:26 +0000

    update

---
+++ run.sh
@@ -0,0 +1,6 @@
+#sudo apt update
+#sudo apt-get install openjdk-8-jre-headless
+
+#npm i ngrok --save-dev
+
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar -Xms1024m -Xmx6144m /workspaces/MCpixelmon/forge-1.12.2-14.23.5.2859.jar
