69f0b8ea9c01573438d5fc10dd18900682fbcdc7
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2024-01-19 17:34:46 +0000

    update

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
-./node_modules/.bin/ngrok config add-authtoken 2b4pxijYq22mOZT3wQexCKuPX4j_2L7aCWNfKXCoaaoVkhA9p
-# ./node_modules/.bin/ngrok config add-authtoken 2b5CjFWtALNkAei5junEwN0qLqg_79sJeACX1cp856FYWLN4n
+#./node_modules/.bin/ngrok config add-authtoken 2b4pxijYq22mOZT3wQexCKuPX4j_2L7aCWNfKXCoaaoVkhA9p
+./node_modules/.bin/ngrok config add-authtoken 2b5CjFWtALNkAei5junEwN0qLqg_79sJeACX1cp856FYWLN4n
 # ./node_modules/.bin/ngrok config add-authtoken 2b5D8zQ3IAsvLtKuI7Xc3z5qm63_4Z2hXXtFsrEwoaMsdMLHy
 # ./node_modules/.bin/ngrok config add-authtoken 2b5DMKAE61y6uSAzndr8XqaJGXA_2Nf5zQXC1bihyEqeq4MNS
 # ./node_modules/.bin/ngrok config add-authtoken 2b5DSLZOp7xgPSS4ibX63bc97vm_3f7j6ogVAbBVudbGCkwj8
