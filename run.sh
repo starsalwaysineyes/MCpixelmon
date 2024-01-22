004261692f26f13181e8bfe2f3cb41b940703cf6
DawnAurora1314 <157210398+DawnAurora1314@users.noreply.github.com>
2024-01-22 16:58:35 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #sudo apt update
 #sudo apt-get install openjdk-8-jre-headless
-
+#sudo apt-get install screen
 #npm i ngrok --save-dev
 
 /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar -Xms1024m -Xmx6144m /workspaces/MCpixelmon/forge-1.12.2-14.23.5.2859.jar nogui
